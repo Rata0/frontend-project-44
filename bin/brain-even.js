955326b2788279ec8abb395f6e2125f04ca476e3
enid <nekon689gmail.com>
2022-11-30 20:28:01 +0300

    add brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+//const parityCheckGame
+const evenOrOdd = () => {
+  const randomNumber = Math.floor(Math.random() * 101);
+  let reply = '';
+  if (randomNumber % 2 === 0) {
+    reply = 'yes';
+  } else {
+    reply = 'no';
+  }
+  return `${randomNumber} - ${reply}`
+};
