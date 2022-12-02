c88dae845978b3e340932620f91915def135807f
enid <nekon689gmail.com>
2022-12-02 16:01:44 +0300

    add index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+import isWelcomeUser from './cli.js';
+
+isWelcomeUser();
+
+const gamebase = (assignment, gameSelected) => {
+  for (let i = 3; i > 0; i -= 1) {
+    
+  }
+};
+
+export default gameBase;
